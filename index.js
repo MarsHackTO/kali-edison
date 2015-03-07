18785ab32913bb93e2d9fa63c9c2821349a40d6e
Zachary Levy <zacharyalevy@gmail.com>
2015-03-07 17:56:47 -0500

    debug

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ console.log("Boot Anti Rape");
 
 var noble = require('noble');
 
+console.log("Noble included");
+
 noble.on('discover', function(peripheral) {
   peripheral.connect(function(error) {
     console.log('connected to peripheral: ' + peripheral.uuid);
