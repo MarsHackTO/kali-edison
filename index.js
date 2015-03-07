7fb700fb9c968768262de9600a94ebd6ffb15324
Zachary Levy <zacharyalevy@gmail.com>
2015-03-07 17:54:16 -0500

    init

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Boot Anti Rape");
+
+noble.on('discover', function(peripheral) {
+  peripheral.connect(function(error) {
+    console.log('connected to peripheral: ' + peripheral.uuid);
+  });
+});
+
+console.log("End Anti Rape");
