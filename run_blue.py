ed060d3d3b6833a6a58b86b430707325b61bc913
Zachary Levy <zacharyalevy@gmail.com>
2015-03-07 19:07:16 -0500

    python init

---
+++ run_blue.py
@@ -0,0 +1,18 @@
+# file: inquiry.py
+# auth: Albert Huang <albert@csail.mit.edu>
+# desc: performs a simple device inquiry followed by a remote name request of
+#       each discovered device
+# $Id: inquiry.py 401 2006-05-05 19:07:48Z albert $
+#
+
+import bluetooth
+
+print("performing inquiry...")
+
+nearby_devices = bluetooth.discover_devices(duration=2, lookup_names=True, lookup_class=False)
+
+print("found %d devices" % len(nearby_devices))
+
+for addr, name in nearby_devices:
+    print("  %s - %s" % (addr, name))
+
